08ca8e4bdc269d096632df92a3bca082f9e51491
whatshu <shussm@qq.com>
2025-11-08 14:55:48 +0800

    working with 2 cores

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,11 @@ use embassy_rp::gpio::{Level, Output};
 use embassy_rp::multicore::{spawn_core1, Stack};
 use embassy_rp::peripherals::PIN_25;
 use embassy_time::{Duration, Timer};
+use static_cell::StaticCell;
 use {defmt_rtt as _, panic_probe as _};
 
 static mut CORE1_STACK: Stack<4096> = Stack::new();
+static EXECUTOR1: StaticCell<embassy_executor::Executor> = StaticCell::new();
 
 #[embassy_executor::task]
 async fn blink_task(pin: PIN_25) {
@@ -26,6 +28,22 @@ async fn blink_task(pin: PIN_25) {
     }
 }
 
+#[embassy_executor::task]
+async fn core1_task() {
+    let p = unsafe { embassy_rp::Peripherals::steal() };
+    let mut led = Output::new(p.PIN_0, Level::Low);
+
+    loop {
+        info!("Core 1: LED ON");
+        led.set_high();
+        Timer::after(Duration::from_millis(500)).await;
+
+        info!("Core 1: LED OFF");
+        led.set_low();
+        Timer::after(Duration::from_millis(500)).await;
+    }
+}
+
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
@@ -35,19 +53,11 @@ async fn main(spawner: Spawner) {
 
     #[allow(static_mut_refs)]
     spawn_core1(p.CORE1, unsafe { &mut CORE1_STACK }, move || {
-        let p = unsafe { embassy_rp::Peripherals::steal() };
-        let mut led = Output::new(p.PIN_0, Level::Low);
-
+        let executor1 = EXECUTOR1.init(embassy_executor::Executor::new());
         info!("Hello from core 1!");
-        loop {
-            info!("Core 1: LED ON");
-            led.set_high();
-            cortex_m::asm::delay(60_000_000);
-
-            info!("Core 1: LED OFF");
-            led.set_low();
-            cortex_m::asm::delay(60_000_000);
-        }
+        executor1.run(|spawner| {
+            spawner.spawn(core1_task()).unwrap();
+        });
     });
 }
 
